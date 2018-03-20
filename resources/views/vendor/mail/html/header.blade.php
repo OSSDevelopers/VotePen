e4fafa80cfb1fcd9482ba6764017f7fad932a9fc
Yoginth <yoginth@zoho.com>
2018-03-20 08:14:08 +0000

    Updated CDN Version

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}">
-            <img src="https://cdn.jsdelivr.net/npm/cdn-votepen@1.0.0/imgs/votepen-email-header.png" width="150" alt="VotePen">
+            <img src="https://cdn.jsdelivr.net/npm/cdn-votepen@1.0.3/imgs/votepen-email-header.png" width="150" alt="VotePen">
         </a>
     </td>
 </tr>
